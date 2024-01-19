760825fe926a33ee06444491cffe3a890014683f
AriannaRonci <s1093926@studenti.univpm.it>
2024-01-19 14:59:21 +0100

    associazione etichette

--- progetto.m
+++ progetto.m
@@ -10,10 +10,10 @@ for i = 1:numel(files)
     data{i} = readtable(filePath);
 end
 
-labelsFile = 'labels.xlsx';
+labelsFile = 'dataset/train/labels.xlsx';
 labelsTable = readtable(labelsFile);
 labelsTable = renamevars(labelsTable,["Var1","Var2","Var3"],["Case","Spacecraft","Condition"]);
-labels = labelsTable.Condition; % Assumendo che la colonna con le etichette si chiami "Label"
+labels = labelsTable.Condition; 
 
 
 X = data;
@@ -25,14 +25,13 @@ labels(strcmp(labels, 'Fault')) = {'1'};
 labels = str2double(labels);
 
 % associo a ogni campione l'etichetta abnormal o normal
-labeledData = cell(numel(X), 3);
+labeledData = cell(numel(X),6);
 
-
-% associo a ogni campione l'etichetta bubble, solenoid o unknown
 for i = 1:numel(X)
     labeledData{i, 1} = X{i};
     labeledData{i, 2} = labels(i);
 
+    % Task2: associo a ogni campione l'etichetta bubble, solenoid o unknown
     if labelsTable{i,"SV1"}~=100 || labelsTable{i,"SV2"}~=100 || labelsTable{i,"SV3"}~=100 || labelsTable{i,"SV4"}~=100
         labeledData{i, 3} = 3;
     elseif labelsTable{i,"BP1"}=="Yes" || labelsTable{i,"BP2"}=="Yes" || labelsTable{i,"BP3"}=="Yes" || labelsTable{i,"BP4"}=="Yes" || labelsTable{i,"BP5"}=="Yes" || labelsTable{i,"BP6"}=="Yes" || labelsTable{i,"BP7"}=="Yes" || labelsTable{i,"BV1"}=="Yes"
@@ -43,8 +42,28 @@ for i = 1:numel(X)
         labeledData{i, 3} = 1;
     end
 
-    
+    % Task3:associo a ogni campione l'etichetta BV1 o BP1-BP7
+    if labelsTable{i,"BP1"} == "Yes"
+        labeledData{i,4} = 1;
+    elseif labelsTable{i,"BP2"} == "Yes"
+        labeledData{i,4} = 2;
+    elseif labelsTable{i,"BP3"} == "Yes"
+        labeledData{i,4} = 3;
+    elseif labelsTable{i,"BP4"} == "Yes"
+        labeledData{i,4} = 4;
+    elseif labelsTable{i,"BP5"} == "Yes"
+        labeledData{i,4} = 5;
+    elseif labelsTable{i,"BP6"} == "Yes"
+        labeledData{i,4} = 6;
+    elseif labelsTable{i,"BP7"} == "Yes"
+        labeledData{i,4} = 7;
+    elseif labelsTable{i,"BV1"} == "Yes"
+        labeledData{i,4} = 8;
+    else
+        labeledData{i,4} = 0;
+    end
 
+    % Task4: associo a ogni campione l'etichetta SV1-SV4 
     if labelsTable{i,'SV1'}~=100
         labeledData{i,5} = 1;
     elseif labelsTable{i,'SV2'}~=100
@@ -57,12 +76,20 @@ for i = 1:numel(X)
         labeledData{i,5} = 0;
     end
 
+    % Task4: associo a ogni campione l'etichetta
+    if labelsTable{i,'SV1'}~=100
+        labeledData{i,6} = labelsTable{i,'SV1'};
+    elseif labelsTable{i,'SV2'}~=100
+        labeledData{i,6} = labelsTable{i,'SV2'};
+    elseif labelsTable{i,'SV3'}~=100
+        labeledData{i,6} = labelsTable{i,'SV3'};
+    elseif labelsTable{i,'SV4'}~=100
+        labeledData{i,6} = labelsTable{i,'SV4'};
+    else
+        labeledData{i,6} = 100;
+    end
+
 end
 
-% associo a ogni campione l'etichetta BV1 o BP1-BP7
-
-% associo a ogni campione l'etichetta SV1-SV4 
-
 labeledData = cell2table(labeledData);
-
 %diagnosticFeatureDesigner(X, labels);
