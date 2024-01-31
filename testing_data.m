2d2d420fc774139a6ffc72b7174153d78408dce0
chiara <s1093786@studenti.univpm.it>
2024-01-31 16:21:21 +0100

    testing

--- testing_data.m
+++ testing_data.m
@@ -1,10 +1,8 @@
-numWindow = 19;
-
-[yfit,scores]=ensemble25.predictFcn(testTable);
-
+numWindow = 10;
+[yfit,scores]=trainedModel.predictFcn(testTable);
 len = length(yfit);
 
-labels = testTable.Task1;
+labels = testTable.Task2;
 label_array = [];
 
 for i = 1:numWindow:len-numWindow+1
@@ -13,29 +11,55 @@ end
 
 prediction = [];
 
-for i = 1:numWindow:len-numWindow+1
-    countOfOnes = sum(yfit(i:i+numWindow-1) == 1);
-    countOfZeros = numWindow-countOfOnes;
-    if countOfOnes>=12
-        prediction = [prediction, 1];
-    else
-        prediction = [prediction, 0];
+if ismember('Task1', FeatureTable1.Properties.VariableNames)
+    
+    for i = 1:numWindow:len-numWindow+1
+        countOfOnes = sum(yfit(i:i+numWindow-1) == 1);
+        countOfZeros = numWindow-countOfOnes;
+        if countOfOnes>=10
+            prediction = [prediction, 1];
+        else
+            prediction = [prediction, 0];
+        end
     end
+    
+    
+    correctPredictions = label_array == prediction;
+    
+    % Calculate accuracy
+    accuracy = sum(correctPredictions) / numel(label_array);
+    
+    % Display accuracy
+    disp(['Accuracy: ', num2str(accuracy * 100), '%']);
+    
+    classLabels = {'Normal', 'Abnormal'};
+    
+    C = confusionmat(label_array,prediction);
+    confusionchart(C, classLabels)
+
+elseif ismember('Task2', FeatureTable1.Properties.VariableNames)
+    for i = 1:numWindow:len-numWindow+1
+        countOfTwo = sum(yfit(i:i+numWindow-1) == 2);
+        countOfThree = numWindow-countOfTwo;
+        if countOfTwo>=10
+            prediction = [prediction, 2];
+        else
+            prediction = [prediction, 3];
+        end
+    end
+    
+    
+    correctPredictions = label_array == prediction;
+    
+    % Calculate accuracy
+    accuracy = sum(correctPredictions) / numel(label_array);
+    
+    % Display accuracy
+    disp(['Accuracy: ', num2str(accuracy * 100), '%']);
+    
+    classLabels = {'Solenoid Valve Fault', 'Bubble Anomaly'}
+    
+    C = confusionmat(label_array,prediction)
+    confusionchart(C, classLabels)
 end
-
-
-correctPredictions = label_array == prediction;
-
-% Calculate accuracy
-accuracy = sum(correctPredictions) / numel(label_array);
-
-% Display accuracy
-disp(['Accuracy: ', num2str(accuracy * 100), '%']);
-
-C = confusionmat(label_array,prediction)
-confusionchart(C)
-
-
-
-
-
+    
