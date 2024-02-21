5bbd6a008bbb6ca6e93822a9224749943f8cbd04
chiara <s1093786@studenti.univpm.it>
2024-02-21 18:49:08 +0100

    testing

---
+++ testing/calculate_accuracy.m
@@ -0,0 +1,11 @@
+function [accuracy, C] = calculate_accuracy(actual, predicted, labels, toDelete)
+table = [actual predicted];
+table(ismember(table(:,1), toDelete),:) = [];
+table(ismember(table(:,2), toDelete),:) = [];
+
+correctPredictions = table(:,1) == table(:,2);
+accuracy = sum(correctPredictions) / length(table);
+
+C = confusionmat(table(:,1),table(:,2));
+end
+
