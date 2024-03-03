cb2efac24d51f75aad0e3582b79bc025067d055d
FedericaParlapiano <phede26@gmail.com>
2024-03-03 22:27:32 +0100

    immagine

--- testing/tasks.m
+++ testing/tasks.m
@@ -343,7 +343,7 @@ hold on;
 xlabel('sample')
 ylabel('opening ratio')
 legend('predicted', 'true')
-title(['Scatter plot Task 5']);
+title(['Scatter plot']);
 subtitle(['RMSE: ', num2str(RMSE_median), newline, 'MAE: ', num2str(MAE)])
 
 %% final score
